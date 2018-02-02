07425bd89bc8f81b46085dae1a152c5b31d1d3da
Pia Sommerauer <piasommerauer@Pias-MacBook-Pro.local>
2018-02-02 17:17:56 +0100

    added to do list to preprocessing script

--- preprocessing/data_preparation_with_spacy.py
+++ preprocessing/data_preparation_with_spacy.py
@@ -1,2 +1,11 @@
 import sys
-import space
+import spacy
+
+# Read in raw data (definitions corpus for training/tesintg?)
+# pass to spacy
+# extract depenency relations
+# find classifier
+# dependency output --> features for classifier
+# Other features?
+# Train classifier
+# Evaluate classifier
